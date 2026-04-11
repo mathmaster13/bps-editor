8d81faa35267c0dd40025e5633b12673e6d18204
Sean <74142852+mathmaster13@users.noreply.github.com>
2026-04-11 04:28:47 -0400

    Update bps.js

--- bps.js
+++ bps.js
@@ -1,9 +1,20 @@
-/* BPS module for Rom Patcher JS v20240821 - Marc Robledo 2016-2024 - http://www.marcrobledo.com/license */
+/* BPS module for Rom Patcher JS v20240821 - Marc Robledo 2016-2024 - MIT License, http://www.marcrobledo.com/license */
+/* escapeHtml Copyright(c) 2012-2013 TJ Holowaychuk
+ * Copyright(c) 2015 Andreas Lubbe
+ * Copyright(c) 2015 Tiancheng "Timothy" Gu 
+ * MIT License */
 /* File format specification: https://www.romhacking.net/documents/746/ */
+
+/* Changes by mathmaster13, Copyright(c) 2026, MIT License */
 
 /* MIT License
 
-Copyright (c) 2017-2024 Marc Robledo
+bps.js Copyright (c) 2017-2024 Marc Robledo
+
+escape-html:
+Copyright (c) 2012-2013 TJ Holowaychuk
+Copyright (c) 2015 Andreas Lubbe
+Copyright (c) 2015 Tiancheng "Timothy" Gu
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -39,12 +50,59 @@ BPS.prototype.toString=function(){
 	return s
 }
 
+function escapeHtml (string) {
+  var str = '' + string
+  var match = matchHtmlRegExp.exec(str)
+
+  if (!match) {
+    return str
+  }
+
+  var escape
+  var html = ''
+  var index = 0
+  var lastIndex = 0
+
+  for (index = match.index; index < str.length; index++) {
+    switch (str.charCodeAt(index)) {
+      case 34: // "
+        escape = '&quot;'
+        break
+      case 38: // &
+        escape = '&amp;'
+        break
+      case 39: // '
+        escape = '&#39;'
+        break
+      case 60: // <
+        escape = '&lt;'
+        break
+      case 62: // >
+        escape = '&gt;'
+        break
+      default:
+        continue
+    }
+
+    if (lastIndex !== index) {
+      html += str.substring(lastIndex, index)
+    }
+
+    lastIndex = index + 1
+    html += escape
+  }
+
+  return lastIndex !== index
+    ? html + str.substring(lastIndex, index)
+    : html
+}
+
 BPS.prototype.fullString=function(){
 	let s = `<p>Source size: ${this.sourceSize}
 	<br>
 	Target size: ${this.targetSize}
 	<br>
-	${this.metaData.length === 0 ? "This patch has no metadata." : "Metadata: "}${this.metaData}
+	${this.metaData.length === 0 ? "This patch has no metadata." : "Metadata: "}${escapeHtml(this.metaData)}
 	<br>
 	#Actions: ${this.actions.length}
 	<br>
